492632cd322e103930b0cf138703483663dea6a4
PetiKVX <petikvx@gmail.com>
2025-03-10 22:48:29 +0100

    add malware_win_chaos_kaspersky from mwdb

---
+++ HEUR-Trojan-PSW/MSIL/HEUR-Trojan-PSW.MSIL.Azorult.gen-68ec37ccb2e6682f6f444b13b9d6f0098ef45774dcf856328d7c9af440891679/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("VLC media player")]
+[assembly: AssemblyCopyright("Copyright © 1996-2018 VideoLAN and VLC Author")]
+[assembly: AssemblyTitle("VLC media player")]
+[assembly: AssemblyDescription("VLC media player")]
+[assembly: AssemblyCompany("VLC media player")]
+[assembly: AssemblyFileVersion("3.0.3.0")]
+[assembly: Guid("097d1b9c-fa02-4bc9-ad03-19b20b9ea948")]
+[assembly: AssemblyTrademark("VLC media player, VideoLAN and x264 are registered trademarks from VideoLAN")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.0.3.0")]
